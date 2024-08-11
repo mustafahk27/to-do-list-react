d4142cb0b922e782808cf14d68ccff5f82ebca1a
mustafa <i.mustafahk@gmail.com>
2024-08-11 22:30:36 +0400

    2nd commit

--- src/components/ToDoWrapper.js
+++ src/components/ToDoWrapper.js
@@ -1,11 +1,20 @@
 import React, {useState} from 'react'
 import ToDoForm from './ToDoForm'
+import Todo from './Todo'
+import { v4 as uuidv4} from 'uuid'
+uuidv4();
 
 const ToDoWrapper = () => {
     const[todos, setTodos] = useState([])
+
+    const addTodo = todo => {
+        setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}])
+        console.log(todos)
+    }
   return (
     <div className='TodoWrapper'>
-      <ToDoForm/>
+      <ToDoForm addTodo = {addTodo}/>
+      <Todo />
     </div>
   )
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 const Todo = () => {
   return (
-    <div>
-      
+    <div className='Todo'>
+      <p>Go to School</p>
+        <div>
+            <FontAwesomeIcon icon = {faPenToSquare}/>
+            <FontAwesomeIcon icon = {faTrash}/>
+        </div>
     </div>
   )
 }
