dca6acb58048414d1c29ce7f8f0607b1a748d23e
mustafa <i.mustafahk@gmail.com>
2024-08-12 22:47:26 +0400

    edit todo p1

--- src/components/EditToDoForm.js
+++ src/components/EditToDoForm.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const EditToDoForm = () => {
+const EditToDoForm = ({editTodo, task}) => {
+    const [value, setValue] = useState("")
+    const handleSubmit = e=> {
+        e.preventDefault();
+        editTodo(value, task.id);
+        setValue("")
+    }
   return (
-    <div>
-      
-    </div>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type='text' className='todo-input' value = {value} placeholder='Update Task' onChange={(e) => setValue(e.target.value)}/>
+        <button type='submit' className='todo-btn'>Update Task</button>
+    </form>
   )
 }
 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,13 +3,14 @@ import ToDoWrapper from './ToDoWrapper'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import EditToDoForm from './EditToDoForm'
 
-const Todo = ({task, toggleComplete, deleteToDo}) => {
+const Todo = ({task, toggleComplete, deleteToDo, editTodo}) => {
   return (
     <div className='Todo'>
       <p onClick = {() => toggleComplete(task.id)} className={`${task.completed ? 'completed': ""}`}>{task.task}</p>
         <div>
-            <FontAwesomeIcon icon = {faPenToSquare}/>
+            <FontAwesomeIcon icon = {faPenToSquare} onClick={() => editTodo(task.id)}/>
             <FontAwesomeIcon icon = {faTrash} onClick={()=> deleteToDo(task.id)}/>
         </div>
     </div>
